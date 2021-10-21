a0d003e1b8544748ee7cfdcc9553bc43823e6912
g hans <gehans@outlook.de>
2021-10-21 12:43:04 +0200

    rn test2

--- reactNative/SharedPreferencesView.js
+++ reactNative/SharedPreferencesView.js
@@ -1,14 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'react-native';
+import { ActivityIndicator, Button, Text } from 'react-native';
+import generatedData from './generated.json';
+import DefaultPreference from 'react-native-default-preference';
+import now from 'performance-now';
 
 const SharedPreferencesView = () => {
-  const [jsonData, setJsonData] = useState();
+  const [time, setTime] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {}, []);
+  const startTest = () => {
+    setLoading(true);
+    let t0 = now();
+    generatedData.map(async e => {
+      await DefaultPreference.set(e.key, e.value);
+    });
+    setTime(now() - t0);
+    setLoading(false);
+  };
 
   return (
     <>
-      <Button title={'START TEST'} />
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <>
+          <Button title={'START TEST'} onPress={() => startTest()} />
+          {time !== 0 ? (
+            <>
+              <Button
+                title={'CLEAR MEMORY'}
+                onPress={async () => {
+                  setLoading(true);
+                  setTime(0);
+                  await DefaultPreference.clearAll();
+                  setLoading(false);
+                }}
+              />
+
+              <Text>{`Stored ${generatedData.length} Key-Value Pairs in ${time} milliseconds`}</Text>
+            </>
+          ) : null}
+        </>
+      )}
     </>
   );
 };
